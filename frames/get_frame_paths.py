bd91f4eae2b41bfb85874c889a9df9b6059a63e8
PanayiotaK <panagiota.konstantinou@durham.ac.uk>
2022-07-28 22:45:20 +0100

    export tsv file with frames paths for VQGAN

---
+++ frames/get_frame_paths.py
@@ -0,0 +1,28 @@
+import os
+import csv
+import pandas as pd
+import numpy as np
+import argparse
+
+
+def main(origin_path,out_file_name):
+  out_file = os.path.join(origin_path, out_file_name)
+  with open(out_file, 'wt', encoding='utf-8') as f:  
+    tsv_writer = csv.writer(f, delimiter='\t')     
+    for categories, subdirs, files in os.walk(origin_path): # "/content/drive/MyDrive/train_vid" 
+          # print(subdir)  
+          for subdir in subdirs:
+            if "frames_" in subdir:            
+              p = os.path.join(categories, subdir)
+              listOfFile = os.listdir(p)              
+              for entry in listOfFile:            
+                fullPath = os.path.join(p, entry)                
+                tsv_writer.writerow([fullPath])
+
+
+if __name__ == '__main__':
+    p = argparse.ArgumentParser()
+    p.add_argument('--origin_path', type=str, default=None, help='Path to all the videos')   
+    p.add_argument('--out_file_name', type=str, default='data_paths.tsv', help='Name of the tsv with all the frame paths will be saved' )
+    
+    main(**vars(p.parse_args()))
